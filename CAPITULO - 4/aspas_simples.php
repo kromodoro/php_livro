3f741bd0c0cf6ef023814c937fc97826760abf6e
kromodoro <kromodoro@gmail.com>
2019-02-11 19:00:10 -0200

    Update aspas_simples.php
    Correção de português

--- CAPITULO - 4/aspas_simples.php
+++ CAPITULO - 4/aspas_simples.php
@@ -8,10 +8,10 @@
 	echo '<p align=center> texto utilizando aspas simples</p>';
 
 	/*
-		Porém devemos tomar cuidado com textos que possuem o caractere (') em sua sequência de caracteres,c omo, por exemplo, o seguinte:
+		Porém devemos tomar cuidado com textos que possuem o caractere (') em sua sequência de caracteres, como, por exemplo, o seguinte:
 
 		'Welcome the jhon's page'
 
-		Como existe um aspa semplies no meio da sequência de caracteres, o PHP vai interpretá-la como delimitador de finalização do texto, e isso causará um erro de execução. Essa problema pode ser facilmente resolvido, inserindo antes da aspa o carctere de controle \ (barra invertida), que indica ao PHP que aquela aspa deve ser tratada como um texto comum, e não como um delimitador.
+		Como existe um aspa simples no meio da sequência de caracteres, o PHP vai interpretá-la como delimitador de finalização do texto, e isso causará um erro de execução. Essa problema pode ser facilmente resolvido, inserindo antes da aspa o carctere de controle \ (barra invertida), que indica ao PHP que aquela aspa deve ser tratada como um texto comum, e não como um delimitador.
 	*/
  ?>
